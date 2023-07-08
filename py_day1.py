819698d9c7be7ff58fabc406483f2729d4c7024f
Aryan-kanse <138276082+Aryan-kanse@users.noreply.github.com>
2023-07-08 12:42:00 +0530

    Add files via upload

---
+++ py_day1.py
@@ -0,0 +1,70 @@
+# assignment 1
+# print("Enter Marks obtained in 3 subjects: ")
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+# sum = n1+n2+n3
+# avg = (sum/3)
+# print("The Average of n1,n2,n3 is",avg)
+
+# assignment 2
+# arithmetic operator
+a=30
+b=40
+print("Addition is: ",a+b)
+print("Subtraction is: ",a-b)
+print("Multiplication is: ",a*b)
+print("Division is: ",a/b)
+print("Mod is: ",a%b)
+print("Floor Division is: ",a//b)
+print("Exponentation is: ",a**b)
+
+# assignment operator
+a=40 ; a+=10
+print("+=",a)
+a=40  ; a-=10
+print("-=",a)
+a=40 ;a*=10
+print("*=",a)
+a=40 ;a/=10
+print("/=",a)
+a=40 ; a//=10
+print("//=",a)
+a=40 ; a%=10
+print("%=",a)
+a=40 ; a**=10
+print("**=",a)
+
+# comparision operator
+a=15 ; b=10
+print("15>=10",a>=b)
+print("15<=10",a<=b)
+print("15==10",a==b)
+print("15!=10",a!=b)
+print("15<10",a<b)
+print("15>10",a>b)
+
+# logical operator
+a=False ; b=True
+print("AND is:",a and b)
+print("OR is:",a or b)
+print("NOT is:",not b)
+
+
+# bitwise operator
+a=30 ;b=20
+print("Bitwise & is:",a&b)
+print("Bitwise | is:",a|b)
+print("Bitwise ^ is:",a^b)
+print("Bitwise >> is:",a>>2)
+print("Bitwise << is:",a<<2)
+
+#identity operator
+a=20 ; b=20.0
+print("Is:",a is b)
+print("Is not:",a is not b)
+
+# membership operators
+a="Aryan"
+print("In","A" in a)
+print("Not in","K" not in a)
